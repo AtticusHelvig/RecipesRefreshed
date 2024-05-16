0e2f1e284ef166c5c9ca16f792e192fea5dd7df5
Atticus <ajhelvig@proton.me>
2024-05-16 11:33:10 -0700

    Version 1.0.0

--- src/main/java/net/atticus/recipes_refreshed/RecipesRefreshedClient.java
+++
@@ -1,9 +0,0 @@
-package net.atticus.recipes_refreshed;
-
-import net.fabricmc.api.ClientModInitializer;
-
-public class RecipesRefreshedClient implements ClientModInitializer {
-    @Override
-    public void onInitializeClient() {
-    }
-}
