e2702a9519eeb1728c5dcfeabb1e6063fa3c40bd
Atticus <ajhelvig@proton.me>
2024-05-16 07:50:07 -0700

    first commit

---
+++ src/main/java/net/atticus/recipes_refreshed/RecipesRefreshedClient.java
@@ -0,0 +1,9 @@
+package net.atticus.recipes_refreshed;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RecipesRefreshedClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/net/atticus/recipes_refreshed/RecipesRefreshedDataGenerator.java
@@ -0,0 +1,11 @@
+package net.atticus.recipes_refreshed;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class RecipesRefreshedDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
